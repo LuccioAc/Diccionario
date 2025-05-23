d82509990d362b37656ca9ffd1aedf7217da6456
LuccioAc <148603107+LuccioAc@users.noreply.github.com>
2025-05-22 23:10:26 -0600

    Stashed

--- dictapi/obj/Debug/net8.0/dictapi.AssemblyInfo.cs
+++ dictapi/obj/Debug/net8.0/dictapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dictapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cfa5382a4683ac6c8e4be42639ce55fc149c918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95312ff50644141795198c2245c07af778a7b6e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dictapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dictapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
